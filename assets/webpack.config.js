64f2fdedad3f54d750a6f3d15ce9fca7697faf47
Guillermo Gutierrez <guillegr123@gmail.com>
2020-05-28 16:37:25 -0600

    Set up JSX support via Babel

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -27,7 +27,10 @@ module.exports = (env, options) => {
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.jsx?$/,
+          resolve: {
+            extensions: ['.js', '.jsx']
+          },
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader'
